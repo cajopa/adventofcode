905e5a4eb5194aa73fba68a53bbfef9a7501b3b9
Carl Johnson <carl@protectamerica.com>
2017-12-20 15:59:51 -0600

    2017 day16 part2 - seems to work, not performant enough to get a result this year

--- 2017/day16.py
+++ 2017/day16.py
@@ -7,21 +7,42 @@ DEBUG = True
 def load(input_filename='day16.input'):
     with open(input_filename, 'r') as f:
         for chunk in f.read().strip().split(','):
+            if DEBUG:
+                print 'loading {}'.format(chunk)
+            
             match = re.match(r's(?P<amount>\d+)', chunk)
             if match:
-                yield lambda word: spin(word, int(match.groupdict()['amount']))
+                groupdict = match.groupdict()
+                amount = int(groupdict['amount'])
+                
+                if DEBUG:
+                    print 'matched s: {}'.format(groupdict)
+                
+                yield lambda word: spin(word, amount)
                 continue
             
             match = re.match(r'x(?P<pos1>\d+)/(?P<pos2>\d+)', chunk)
             if match:
                 groupdict = match.groupdict()
-                yield lambda word: exchange(word, int(groupdict['pos1']), int(groupdict['pos2']))
+                pos1 = int(groupdict['pos1'])
+                pos2 = int(groupdict['pos2'])
+                
+                if DEBUG:
+                    print 'matched x: {}'.format(groupdict)
+                
+                yield lambda word: exchange(word, pos1, pos2)
                 continue
             
             match = re.match(r'p(?P<name1>[a-z])/(?P<name2>[a-z])', chunk)
             if match:
                 groupdict = match.groupdict()
-                yield lambda word: partner(word, groupdict['name1'], groupdict['name2'])
+                name1 = groupdict['name1']
+                name2 = groupdict['name2']
+                
+                if DEBUG:
+                    print 'matched p: {}'.format(groupdict)
+                
+                yield lambda word: partner(word, name1, name2)
                 continue
 
 def run1(size=16, steps=None):
@@ -32,6 +53,27 @@ def run1(size=16, steps=None):
     
     for step in steps:
         lineup = step(lineup)
+    
+    return lineup
+
+def run2(size=16, steps=None, rounds=10**9):
+    lineup = get_lineup(size)
+    
+    if steps is None:
+        steps = tuple(load())
+    
+    try:
+        for i in xrange(rounds):
+            if DEBUG and i%1000==0:
+                print 'round {}'.format(i)
+            
+            for step in steps:
+                lineup = step(lineup)
+    except:
+        if DEBUG:
+            import pdb; pdb.post_mortem()
+        else:
+            raise
     
     return lineup
 
