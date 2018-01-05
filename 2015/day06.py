5f7159a6bfc27915cdcc58b01fcfe0436b565a45
Carl Johnson <carl@protectamerica.com>
2018-01-04 18:11:32 -0600

    day06 complete

--- 2015/day06.py
+++ 2015/day06.py
@@ -1,7 +1,20 @@
+from itertools import chain
 import re
 
 
 DEBUG = False
+
+ACTIONS1 = {
+    'turn on': lambda x: True,
+    'turn off': lambda x: False,
+    'toggle': lambda x: (not x)
+}
+
+ACTIONS2 = {
+    'turn on': lambda x: x+1,
+    'turn off': lambda x: max(0, x-1),
+    'toggle': lambda x: x+2
+}
 
 
 def load(input_filename):
@@ -11,32 +24,37 @@ def load(input_filename):
             
             groupdict = match.groupdict()
             
-            if groupdict['action'] == 'turn on':
-                action = lambda x: 1
-            elif groupdict['action'] == 'turn off':
-                action = lambda x: 0
-            elif groupdict['action'] == 'toggle':
-                action = lambda x: 1 - x
-            else:
-                raise ValueError('invalid action {}'.format(groupdict['action']))
-            
             point0 = (int(groupdict['x0']), int(groupdict['y0']))
             point1 = (int(groupdict['x1']), int(groupdict['y1']))
             
-            yield action, point0, point1
+            yield groupdict['action'], point0, point1
 
 def run1(input_filename='input/day06'):
-    return count_lights(follow_instructions(load(input_filename)))
+    return count_lights(follow_instructions(load(input_filename), False, ACTIONS1))
 
-def follow_instructions(instructions):
-    grid = [[0] * 1000] * 1000
+def run2(input_filename='input/day06'):
+    return total_brightness(follow_instructions(load(input_filename), 0, ACTIONS2))
+
+def follow_instructions(instructions, initial_value, actions, size=1000):
+    grid = [[initial_value for _ in xrange(size)] for _ in xrange(size)]
     
+    # import pdb; pdb.set_trace()
     for action, point0, point1 in instructions:
-        for x in xrange(point0[0], point1[0]+1):
-            for y in xrange(point0[1], point1[1]+1):
-                grid[x][y] = action(grid[x][y])
+        minx = min(point0[0], point1[0])
+        maxx = max(point0[0], point1[0])
+        miny = min(point0[1], point1[1])
+        maxy = max(point0[1], point1[1])
+        
+        for x in xrange(minx, maxx+1):
+            for y in xrange(miny, maxy+1):
+                grid[x][y] = actions[action](grid[x][y])
+                if DEBUG:
+                    print x, y, grid[x][y]
     
     return grid
 
 def count_lights(grid):
-    return sum([sum(x) for x in grid])
+    return sum((len(filter(None, col)) for col in grid))
+
+def total_brightness(grid):
+    return sum(chain(*grid))
