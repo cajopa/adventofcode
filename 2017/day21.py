825cb3804d6d9a51fb193a890cdcbc5dbde03e04
Carl Johnson <carl@protectamerica.com>
2018-02-07 14:40:25 -0600

    2017 day 21 part 1 - removed silly Matrix class and implemented rotation and reflection methods on Grid

--- 2017/day21.py
+++ 2017/day21.py
@@ -1,60 +1,20 @@
-from itertools import product
 from collections import defaultdict
-import math
 
 
 DEBUG = True
 
 
-class Matrix:
-    def __init__(self, width, height, values):
-        self.width = width
-        self.height = height
-        self.values = defaultdict(int)
-        
-        for p,v in zip(((x,y) for y in range(height) for x in range(width)), values):
-            self.values[p] = v
-    
-    def __str__(self):
-        max_digits = math.ceil(math.log10(max(self.values.values()) + 1))
-        
-        return '\n'.join(' '.join('{N: {L}d}'.format(N=self.values[x,y], L=max_digits) for x in range(self.width)) for y in range(self.height))
-    
-    def __mul__(self, other):
-        if self.width == other.height:
-            def inner():
-                for x,y in ((x,y) for y in range(self.height) for x in range(other.width)):
-                    yield self.row(y).dot_product(other.column(x))
-            
-            return self.__class__(other.width, self.height, inner())
-        else:
-            raise ValueError('Can only be multiplied by a matrix with {} rows'.format(self.width))
-    
-    def dot_product(self, other):
-        if self.width == 1 and other.width == 1:
-            return sum(x*y for x,y in zip(self.flat, other.flat))
-        else:
-            raise ValueError('Dot product only applies to vectors')
-    
-    def column(self, index):
-        return self.__class__(1, self.height, (self.values[index, i] for i in range(self.height)))
-    
-    def row(self, index):
-        return self.__class__(1, self.width, (self.values[i, index] for i in range(self.width)))
-    
-    @property
-    def flat(self):
-        return (self.values[x,y] for y in range(self.height) for x in range(self.width))
-
-
 class Grid:
-    INITAL_PATTERN = ((False, True, False),
-                      (False, False, True),
-                      (True, True, True))
+    INITAL_PATTERN = (False, True, False,
+                      False, False, True,
+                      True, True, True)
     
     
-    def __init__(self):
-        pass
+    def __init__(self, flat_values):
+        flat_values = list(flat_values)
+        
+        self.size = int(len(flat_values) ** 0.5)
+        self.values = defaultdict(bool, zip(((x,y) for y in range(self.size) for x in range(self.size)), flat_values))
     
     def __iter__(self):
         while True:
@@ -62,13 +22,44 @@ class Grid:
             
             yield self
     
+    def __eq__(self, other):
+        flat_values = self.flat()
+        
+        return (
+            flat_values == self.rotate_clockwise() or
+            flat_values == rotate_counterclockwise() or
+            flat_values == rotate_halfturn() or
+            flat_values == reflect_vertical() or
+            flat_values == reflect_horizontal()
+        )
+    
+    @classmethod
+    def from_text(cls, text):
+        return cls(x == '#' for x in text if x != '/')
+    
     @classmethod
     def combine(self, subgrids):
         pass
     
     ######### basic operations #########
     
+    def flat(self):
+        return [self.values[x,y] for y in range(self.size) for x in range(self.size)]
     
+    def rotate_clockwise(self):
+        return [self.values[x,y] for x in range(self.size) for y in reversed(range(self.size))]
+    
+    def rotate_counterclockwise(self):
+        return [self.values[x,y] for x in reversed(range(self.size)) for y in range(self.size)]
+    
+    def rotate_halfturn(self):
+        return [self.values[x,y] for y in reversed(range(self.size)) for x in reversed(range(self.size))]
+    
+    def reflect_vertical(self):
+        return [self.values[x,y] for y in reversed(range(self.size)) for x in range(self.size)]
+    
+    def reflect_horizontal(self):
+        return [self.values[x,y] for y in range(self.size) for x in reversed(range(self.size))]
     
     ######### 
     
