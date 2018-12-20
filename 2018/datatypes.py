194094b9981d0ae577a6b05cb4b03959bf610749
Carl Johnson IV <carl@protectamerica.com>
2018-12-20 18:07:03 +0000

    implemented frozendict so it can be a key for testing purposes

---
+++ 2018/datatypes.py
@@ -0,0 +1,17 @@
+class frozendict(dict):
+    def __setitem__(self, key, value):
+        if key not in self:
+            super().__setitem__(key, value)
+    
+    def __eq__(self, other):
+        return super().__eq__(other)
+    
+    def __hash__(self):
+        return hash(self.__class__) ^ hash(tuple(sorted(self.items(), key=lambda x: x[0])))
+    
+    def __repr__(self):
+        return 'h' + super().__repr__()
+    
+    def update(self, other):
+        for k,v in other.items():
+            self[k] = v
