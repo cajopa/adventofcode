6b8b588b22185e45c21bf3103e0cf44a31918422
Carl Johnson IV <carl@protectamerica.com>
2018-12-21 21:48:44 +0000

    note for future development

--- 2018/datatypes.py
+++ 2018/datatypes.py
@@ -15,3 +15,10 @@ class frozendict(dict):
     def update(self, other):
         for k,v in other.items():
             self[k] = v
+
+### TODO: dict subclass that keys on an unadic function
+#         should detect (somehow) changes in the "observed value"
+#         a la knockout? (declare observable(s) and computed)
+#         include caching
+#         async
+#         multiple keys returnable?
